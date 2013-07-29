f3e3c9cd235073f587c73b7a6d94058e86e9b42f
forecho <caizhenghai@gmail.com>
2013-07-29 22:15:48 +0800

    换一种写法 - 发一个链接有5点声望

--- protected/controllers/PostsController.php
+++ protected/controllers/PostsController.php
@@ -80,10 +80,8 @@ class PostsController extends Controller
 			
 			if($model->save()){
 				if ($_POST['Posts']['bp_url']) {
-					//发一个链接有5点声望
-					$user=User::model()->findByPk(Yii::app()->user->id);
-					$user->bu_reputation=$user->bu_reputation+'5';
-					$user->save(); // 将更改保存到数据库
+					$record = User::model()->findByPk(Yii::app()->user->id);
+					$record->saveCounters(array('bu_reputation'=>5));
 				}
 				$this->redirect(array('view','id'=>$model->bp_id));
 			}
