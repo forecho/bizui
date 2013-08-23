d35d3dddb2f5caff604125c2a24cca839bac17b1
forecho <caizhenghai@gmail.com>
2013-08-23 15:07:21 +0800

    定时任务ok

--- protected/commands/TestCommand.php
+++ protected/commands/TestCommand.php
@@ -7,21 +7,32 @@ class TestCommand extends CConsoleCommand
      * 在这里写入你的代码逻辑。
      * eg: protected/yiic test index --id=15   
      */
-    public function actionIndex($id='') {
+    public function actionIndex() {
          
         //$model=Posts::model()->findByPk(10);
 
-		// Uncomment the following line if AJAX validation is needed
-		// $this->performAjaxValidation($model);
-		$model = Posts::model()->updateByPk($id, array('bp_score'=>11));
+		//$model = Posts::model()->updateByPk($id, array('bp_score'=>11));
+		$posts = Posts::model()->findAll();
+		foreach ($posts as $key => $value) {
+			//将排名下拉的重力因子
+			//该因子决定了post 的下降速度, 值越高排名下降越快, 时效性的参数之一
+			$rank = 1.8;
+			//获得的投票
+			$vote = $value->bp_like;
+			//数据创建时间
+			$created = $value->bp_create_time;
+			//距离创建时间的小时数, 时效性的参数之二
+			$hourDiffCreated = (time() - $created) / 3600;
+			//详细算法
+			$score = ($vote - 1) / pow(($hourDiffCreated + 2), $rank);
+			$count = Posts::model()->updateByPk($value->bp_id, array('bp_score'=>$score));
+		}
 
-		// if(isset($_POST['Posts']))
-		// {
-			 // $model->bp_score=100;
-			 // $model->save();
-		//}
-         //echo var_dump($model->errors);
-         print_r($model);
+        if ($count) {
+        	echo "Ok";
+        }else{
+        	echo "No";
+        }
     }
          
     /**
